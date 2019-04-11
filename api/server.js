5fc1d392081b2eb6d3144a042bbd4b2c8000e223
jdspell <spelljd15@gmail.com>
2019-04-11 19:54:04 -0400

    added tests to check for response codes and whether data is in json format for delete and post requests

--- api/server.js
+++ api/server.js
@@ -10,4 +10,33 @@ server.get('/', async (req, res) => {
     res.status(200).json({ message: "working" });
 });
 
+server.get('/users', async (req, res) => {
+    try{
+        const users = await db('users');
+        res.status(200).json(users);
+    } catch (error) {
+        res.status(500).json(error);
+    }
+});
+
+server.post('/users', async (req, res) => {
+    try {
+        const [id] = await db('users').insert(req.body);
+        const user = await db('users').where({ id }).first();
+        res.status(200).json(user);
+    } catch (error) {
+        res.status(500).json(error);
+    }
+});
+
+server.delete('/users/:id', async (req, res) => {
+    try {
+        const count = await db('users').where({ id: req.params.id }).delete();
+        res.status(200).json(count);
+    } catch (error) {
+        res.status(500).json(error);
+    }
+});
+
+
 module.exports = server;
